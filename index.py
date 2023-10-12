0540561454ef44923ebfd60521a52e4adcb92a1a
Elwan-51 <78788340+Elwan-51@users.noreply.github.com>
2023-10-12 13:35:51 +0200

    Refactor function (#4)
    * Refactor files in app package
    
    Rename function :
    - WebAurion.base_request
    - WebAurion.get_viewState
    - WebAurion.get_formID
    - JsonManip.get_data
    - Calendar.add_to_calendar_from_webaurion
    - Calendar.event_type_webaurion
    - Calendar.data_with_organizer
    - Calendar.static.name_printable
    - Calendar.print_calendar_to_file
    
    Change class TimeManip do DataClass
    
    * Refactor files in app package
    
    Rename function :
    - WebAurion.base_request
    - WebAurion.get_viewState
    - WebAurion.get_formID
    - JsonManip.get_data
    - Calendar.add_to_calendar_from_webaurion
    - Calendar.event_type_webaurion
    - Calendar.data_with_organizer
    - Calendar.static.name_printable
    - Calendar.print_calendar_to_file
    
    Change class TimeManip do DataClass
    
    * Edit Event info to delete space on it
    
    ---------
    
    Co-authored-by: Matthieu LAPETITTE <58515145+Spiti-gaming@users.noreply.github.com>

--- index.py
+++ index.py
@@ -1,17 +1,14 @@
-from WebAurion import WebAurion
-from Tools import TimeManip, JsonManip
-from Calendar import CalendarTools
+from app.TimeManip import TimeManip2
+from app.JsonManip import JsonManip
+from app.Calendar import CalendarTools
+from app.WebAurion import WebAurion
 import json
 
 
-
-with open("config.json") as json_data_file:
+with open("./config.json") as json_data_file:
     config = json.load(json_data_file)
 
-timestamp = TimeManip(config['webaurion']['years'])
-
-
-
+timestamp = TimeManip2(type_year=config['webaurion']['years'])
 
 MyCpe = WebAurion(config['webaurion']['host'])
 
@@ -20,22 +17,24 @@ MyCpe = WebAurion(config['webaurion']['host'])
 #       Login
 #===============================================================================
 payload = 'username='+config['webaurion']['username']+'&password='+config['webaurion']['password']+'&j_idt27='
-if MyCpe.BaseRequest(payload,"/login",True) != True :
+if not MyCpe.base_request(payload, "/login", True):
     exit("Error in connection")
 
 
 #===============================================================================
 #       Get Planning ID
 #===============================================================================
-payload='form=form&form%3AlargeurDivCenter=1620&form%3AidInit='+ MyCpe.formId+'&form%3Asauvegarde=&form%3Aj_idt817%3Aj_idt820_view=basicDay&form%3Aj_idt834%3Aj_idt838=&form%3Aj_idt834%3Atodos281544_selection=&form%3Aj_idt782%3Aj_idt784_dropdown=1&form%3Aj_idt782%3Aj_idt784_mobiledropdown=1&form%3Aj_idt782%3Aj_idt784_page=0&javax.faces.ViewState='+MyCpe.ViewState+'&form%3Asidebar=form%3Asidebar&form%3Asidebar_menuid=6'
-if MyCpe.BaseRequest(payload,"/faces/MainMenuPage.xhtml",True) != True :
+
+payload = 'form=form&form%3AlargeurDivCenter=1620&form%3AidInit='+ MyCpe.formId+'&form%3Asauvegarde=&form%3Aj_idt817%3Aj_idt820_view=basicDay&form%3Aj_idt834%3Aj_idt838=&form%3Aj_idt834%3Atodos281544_selection=&form%3Aj_idt782%3Aj_idt784_dropdown=1&form%3Aj_idt782%3Aj_idt784_mobiledropdown=1&form%3Aj_idt782%3Aj_idt784_page=0&javax.faces.ViewState='+MyCpe.ViewState+'&form%3Asidebar=form%3Asidebar&form%3Asidebar_menuid=6'
+if not MyCpe.base_request(payload, "/faces/MainMenuPage.xhtml", True):
     exit("Error in connection")
 
 #===============================================================================
 #       Calendar Page
 #===============================================================================
-payload = 'javax.faces.partial.ajax=true&javax.faces.source=form%3Aj_idt118&javax.faces.partial.execute=form%3Aj_idt118&javax.faces.partial.render=form%3Aj_idt118&form%3Aj_idt118=form%3Aj_idt118&form%3Aj_idt118_start='+timestamp.timestampstart+'&form%3Aj_idt118_end='+timestamp.timestampstop+'&form=form&form%3AlargeurDivCenter=1620&form%3AidInit='+MyCpe.formId+'&form%3Adate_input=02%2F10%2F2023&form%3Aj_idt118_view=agendaWeek&form%3AoffsetFuseauNavigateur=-7200000&form%3Aonglets_activeIndex=0&form%3Aonglets_scrollState=0&javax.faces.ViewState='+MyCpe.ViewState
-data_str = MyCpe.BaseRequest(payload,"/faces/Planning.xhtml",False)
+
+payload = 'javax.faces.partial.ajax=true&javax.faces.source=form%3Aj_idt118&javax.faces.partial.execute=form%3Aj_idt118&javax.faces.partial.render=form%3Aj_idt118&form%3Aj_idt118=form%3Aj_idt118&form%3Aj_idt118_start='+timestamp.time_stamp_start+'&form%3Aj_idt118_end='+timestamp.time_stamp_stop+'&form=form&form%3AlargeurDivCenter=1620&form%3AidInit='+MyCpe.formId+'&form%3Adate_input=02%2F10%2F2023&form%3Aj_idt118_view=agendaWeek&form%3AoffsetFuseauNavigateur=-7200000&form%3Aonglets_activeIndex=0&form%3Aonglets_scrollState=0&javax.faces.ViewState='+MyCpe.ViewState
+data_str = MyCpe.base_request(payload,"/faces/Planning.xhtml",False)
 
 json_data = JsonManip(data_str)
 # Extract JSON data from CDATA section
@@ -43,17 +42,15 @@ json_data.extract(r'<update id="form:j_idt118"><!\[CDATA\[(.*?)\]\]></update>',1
 json_data.extract(r'{"events".*}',0)
 
 # Convert JSON data to Python dictionary
-data = json_data.getdata()
-
-
+data = json_data.get_data()
 
 
 Calendar = CalendarTools(config['webaurion']['Autonomie'])
 
 # Loop through the events and add them to the calendar
 
-Calendar.AddToCalendarFromWebAurion(data['events'])
+Calendar.add_to_calendar_from_webaurion(data['events'])
 
 # Write the calendar to a file
-Calendar.printCalendartofile(config['icsfile'])
+Calendar.print_calendar_to_file(config['icsfile'])
 
