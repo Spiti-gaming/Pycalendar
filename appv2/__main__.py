2910199503e2183c64145736e7a713f2a6c18004
Matthieu LAPETITTE <matthieu@lapetitte.fr>
2024-09-18 17:15:28 +0200

    Reformat with IA and add management of ITII event

--- appv2/__main__.py
+++ appv2/__main__.py
@@ -4,10 +4,9 @@ from dataclasses import dataclass, field
 from dateutil import parser
 from datetime import datetime
 import datetime as dt
-from icalendar import Calendar, Event, vCalAddress, vText, Timezone
+from icalendar import Calendar, Event, vCalAddress
 import pytz
-
-
+from icalendar.prop import vDatetime
 
 
 @dataclass
@@ -21,23 +20,18 @@ class EventInfo:
     organizer: vCalAddress or str = field(init=False)
     event_type: str = field(init=False)
 
-
     def __post_init__(self):
         self.dtstart = parser.parse(self.data['date_debut']).astimezone(pytz.timezone('Europe/Paris'))
         self.dtend = parser.parse(self.data['date_fin']).astimezone(pytz.timezone('Europe/Paris'))
         self.location = self.data['ressource']
-        if self.data['description'] is None:
-            self.summary = self.data['favori']["f3"]
-        else:
-            self.summary = self.data['description']
-
+        self.summary = self.data['description'] if self.data['description'] else self.data['favori']["f3"]
         self.event_type = self.data['type_activite']
         self.description = self.data['favori']["f3"]
         self.organizer = self.data['intervenants']
         self.data = None
 
-class CalendarTools:
 
+class CalendarTools:
     def __init__(self, auto=True):
         self.cal = Calendar()
         self.autonomie = auto
@@ -46,10 +40,9 @@ class CalendarTools:
         self.cal.add('X-WR-TIMEZONE', 'Europe/Paris')
 
     def add_to_calendar_from_webaurion(self, json_data):
-
         for event_calendar in json_data:
             if event_calendar['type_activite']:
-                if not event_calendar['ressource'] or "Site" not in event_calendar['ressource'] :
+                if not event_calendar['ressource'] or "Site" not in event_calendar['ressource']:
                     event_info = EventInfo(event_calendar)
                     e = Event()
                     e.add('summary', f"{event_info.summary} - {event_info.event_type}")
@@ -58,7 +51,6 @@ class CalendarTools:
                     e.add('location', event_info.location)
                     e.add('organizer', event_info.organizer)
                     e.add('description', f"{event_info.description}\n{event_info.event_type}")
-
                     if self.autonomie:
                         self.cal.add_component(e)
                     else:
@@ -72,20 +64,36 @@ class CalendarTools:
 
     def get_itii_calendar(self, url, host):
         conn = http.client.HTTPConnection(host)
-
         conn.request("GET", url)
         res = conn.getresponse()
         data = res.read()
         calendar = data.decode("utf-8")
-
         g = Calendar.from_ical(calendar)
+        timezone_paris = pytz.timezone('Europe/Paris')
+
         for com in g.walk():
-            if com.get('summary') and "CPE" not in com.get('summary'):
-                self.cal.add_component(com)
+            if com.name == "VEVENT":
+                dtstart_utc = com.decoded('dtstart').isoformat()
+                dtend_utc = com.decoded('dtend').isoformat()
+
+                # Converting UTC dates to Paris timezone
+                dtstart_paris = parser.parse(dtstart_utc).astimezone(pytz.timezone('Europe/Paris'))
+                dtend_paris = parser.parse(dtend_utc).astimezone(pytz.timezone('Europe/Paris'))
+                e = Event()
+                e.add('summary', com.get("summary"))
+                e.add('dtstart', dtstart_paris)
+                e.add('dtend', dtend_paris)
+                e.add('location', com.get("location"))
+                e.add('organizer', com.get("SUMMARY").split(" - ")[1])
+                e.add('description', com.get("description"))
+
+
+                if com.get('summary') and "CPE" not in com.get('summary'):
+                    print(e)
+                    self.cal.add_component(e)
 
 
 def years():
-
     start = dt.date.fromisoformat(str(datetime.today().year - 1) + '-08-01')
     end = dt.date.fromisoformat(str(datetime.today().year + 1) + '-08-01')
     return start, end
@@ -96,22 +104,21 @@ def week():
     end = start + dt.timedelta(days=20)
     start = start.strftime("%Y-%m-%d")
     end = end.strftime("%Y-%m-%d")
-
     return start, end
 
 
 with open("config.json") as f:
     configs = json.load(f)
+
 for config in configs:
     conn = http.client.HTTPSConnection(config['webaurion']['host'])
-    payload = 'username='+config['webaurion']['username']+'&password='+config['webaurion']['password']
+    payload = 'username=' + config['webaurion']['username'] + '&password=' + config['webaurion']['password']
     headers = {
-      'Content-Type': 'application/x-www-form-urlencoded'
+        'Content-Type': 'application/x-www-form-urlencoded'
     }
     conn.request("POST", "/login", payload, headers)
     res = conn.getresponse()
     data = res.read()
-
     headers['Cookie'] = res.getheader('Set-Cookie').split(";")[0]
 
     if config['webaurion']['years']:
@@ -122,15 +129,15 @@ for config in configs:
     conn.request("GET", f"/mobile/mon_planning?date_debut={start}&date_fin={end}", "", headers)
     res = conn.getresponse()
     data2 = res.read()
-
     data_json = json.loads(data2.decode("utf-8"))
 
     calender = CalendarTools()
-    calendrier = calender.add_to_calendar_from_webaurion(data_json)
+    calender.add_to_calendar_from_webaurion(data_json)
+
     if config.get('itii'):
-        calendrier = calender.get_itii_calendar(config['itii']['url'], config['itii']['host'])
+        calender.get_itii_calendar(config['itii']['url'], config['itii']['host'])
         print("ITII Calendar added")
 
     calender.print_calendar_to_file(config['icsfile'])
-    conn.close()
 
+    conn.close()
